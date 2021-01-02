e30ddad9aa0c86f98e043aa0ec383a1f2e8c886a
Eduardo <edu.ascarvalho@gmail.com>
2021-01-01 23:46:37 -0300

    adicionado animações e slides

---
+++ web/assets/js/main.js
@@ -0,0 +1,12 @@
+if (window.SimpleSlide) {
+  new SimpleSlide({
+    slide: "quote",
+    time: 5000,
+  });
+
+  new SimpleSlide({
+    slide: "portfolio",
+    nav: true,
+    time: 5000,
+  });
+}
