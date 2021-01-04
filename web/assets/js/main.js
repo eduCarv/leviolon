1807ed1d8dd9e2e2bb2ad1bf8e179448f1fe153e
Eduardo <edu.ascarvalho@gmail.com>
2021-01-04 15:51:07 -0300

    Animações e formulários

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -10,3 +10,18 @@ if (window.SimpleSlide) {
     time: 5000,
   });
 }
+
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
+
+if (window.SimpleForm) {
+  new SimpleForm({
+    form: ".formphp", // seletor do formulário
+    button: "#enviar", // seletor do botão
+    erro:
+      "<div id='form-erro'><h2>Erro no envio!</h2><p>Um erro ocorreu, tente para o email contato@leviolon.com.</p></div>", // mensagem de erro
+    sucesso:
+      "<div id='form-sucesso'><h2>Formulário enviado com sucesso</h2><p>Em breve eu entro em contato com você.</p></div>", // mensagem de sucesso
+  });
+}
